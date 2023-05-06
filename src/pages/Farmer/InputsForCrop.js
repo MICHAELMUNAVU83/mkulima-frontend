929c66196c1cd5cc9dbd7a136162a0897a11cc16
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-05-06 14:50:06 +0300

    Added

--- src/pages/Farmer/InputsForCrop.js
+++ src/pages/Farmer/InputsForCrop.js
@@ -4,12 +4,15 @@ import BudPayCheckout from "../BudPayScript.js";
 
 const InputsForCrop = () => {
   const [showOrderModal, setShowOrderModal] = useState(false);
-
-  function payWithBudPay(amount) {
+  const [selectedInput, setSelectedInput] = useState({});
+  const [totalKgs, SetTotalKgs] = useState(1);
+  const [totalPrice, SetTotalPrice] = useState(0);
+  function payWithBudPay(e) {
+    e.preventDefault();
     BudPayCheckout({
       key: "pk_live_0syy9e7bnjeduyf1wzct57rzyndbphtduo6bop", // Replace with your public key
       email: "michaelmunavu83@gmail.com",
-      amount: amount,
+      amount: String(totalPrice),
       first_name: "Michael",
       last_name: "Munavu",
       currency: "KES", // Use GHS for Ghana Cedis or USD for US Dollars
@@ -36,6 +39,10 @@ const InputsForCrop = () => {
   }
   const { name } = useParams();
   const [suggested_inputs, setSuggestedInputs] = useState([]);
+
+  useEffect(() => {
+    SetTotalPrice(Number(totalKgs) * Number(selectedInput.price_per_kg));
+  }, [totalKgs, selectedInput]);
 
   useEffect(() => {
     fetch("http://127.0.0.1:3000/input_supplies")
@@ -95,8 +102,10 @@ const InputsForCrop = () => {
             <div className="flex justify-center">
               <button
                 className="bg-gray-100 gap-2 px-6 py-4 rounded-xl font-bold text-[#3B841F] my-2 justify-center place-content-center flex text-md"
-                onClick={(e) => {
+                onClick={() => {
                   setShowOrderModal(true);
+                  setSelectedInput(input);
+                  SetTotalPrice(input.price_per_kg);
                 }}
               >
                 Buy Now
@@ -111,13 +120,81 @@ const InputsForCrop = () => {
         <div className="fixed kulim-park bg-white shadow-xl h-[700px] my-auto w-[900px] inset-0 bg-opacity z-10 flex flex-col items-center justify-center w-[400px]  mx-auto">
           <div className="flex justify-end w-full p-4">
             <button
-              className="bg-black text-white px-4 py-2 rounded-lg"
-              onClick={() => setShowOrderModal(false)}
+              className=" bg-[#3B841F] text-white px-4 py-2 rounded-lg"
+              onClick={() => {
+                setShowOrderModal(false);
+                SetTotalKgs(1);
+                SetTotalPrice(0);
+
+              }}
             >
               X
             </button>
           </div>
-          <div>bnmk,l.;</div>
+          <div className="flex flex-row justify-center rounded-3xl  gap-4 w-[400px] ">
+            <img
+              src={selectedInput.product_image}
+              alt="tomatoes"
+              className="w-[400px] h-[300px] rounded-t-3xl object-cover"
+            />
+            <div className="p-2  flex flex-col gap-2">
+              <div className="flex justify-between mx-8">
+                <p className="text-xl text-[#000] font-bold ">
+                  {selectedInput.name}
+                </p>
+              </div>
+
+              <div className="flex justify-between w-[300px] mx-8">
+                <p className="text-xl text-[#000]">Price</p>
+                <p className="bg-[#3B841F] gap-2 px-4 py-2 rounded-xl text-white ">
+                  {selectedInput.price_per_kg} per kg
+                </p>
+              </div>
+              <div className="flex justify-between mx-8">
+                <p className="text-xl text-[#000]">Location</p>
+                <p className="bg-[#3B841F] gap-2 px-4 py-2 rounded-xl text-white ">
+                  {selectedInput.location}
+                </p>
+              </div>
+
+              <div className="flex justify-between mx-8">
+                <p className="text-xl text-[#000]">Contact</p>
+                <p className="bg-[#3B841F] gap-2 px-4 py-2 rounded-xl text-white ">
+                  {selectedInput.contact}
+                </p>
+              </div>
+              <p className="w-[300px] mx-auto">
+                Select the number of kgs you want to Buy
+              </p>
+              <select
+                className="w-[300px] h-[50px] focus:outline-none border border-2 rounded-2xl mx-auto"
+                onChange={(e) => {
+                  SetTotalKgs(e.target.value);
+                }}
+              >
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+                <option value="4">Four</option>
+              </select>
+
+              <p className="w-[300px] mx-auto text-center">
+                Total price is {totalPrice.toLocaleString()}Ksh for {totalKgs} kgs
+              </p>
+
+              <div className="flex justify-center">
+                <button
+                  className="bg-gray-100 gap-2 px-6 py-4 rounded-xl font-bold text-[#3B841F] my-2 justify-center place-content-center flex text-md"
+                  onClick={(e) => {
+                    payWithBudPay(e);
+                  }}
+                >
+                  Buy Now
+                </button>
+               
+              </div>
+            </div>
+          </div>
         </div>
       )}
       <h1 className="text-[#3B841F] text-center font-bold text-5xl">
